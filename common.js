e97d655dc8d17764c0843be3de8315414ade8d79
youjingyu <735284268@qq.com>
2017-05-26 11:32:11 +0800

    添加验证码图片

--- common.js
+++ common.js
@@ -6,8 +6,13 @@
 getEle('main').setAttribute('style', 'display: block');
 var scale = document.body.clientWidth / 1565;
 document.getElementsByTagName('body')[0].setAttribute('style', 'height:' + (scale * 3202)  + 'px');
+var Reg = {
+    tel: /^1(3|4|5|7|8)\d{9}$/
+}
 var Dom = {
     tel_input: getEle('tel_input'),
+    valid_num: getEle('valid_num'),
+    valid_num_btn: getEle('valid_num_btn'),
     submit_btn: getEle('submit_btn'),
     modal_getted: getEle('modal_getted'),
     close_getted: getEle('close_getted'),
@@ -17,10 +22,33 @@ var Dom = {
     close_over: getEle('close_over'),
     modal_loading: getEle('modal_loading')
 }
+addTouchEvent(Dom.valid_num_btn, function () {
+    if(!(Reg.tel.test(Dom.tel_input.value))){
+        alert("手机号码有误，请重填");
+        return false;
+    }
+    var btn = Dom.valid_num_btn;
+    btn.setAttribute("disabled", true);
+    btn.setAttribute("style", 'background: none;background-color: grey;border-color: grey');
+    timeDown(btn, 60);
+    function timeDown(obj, wait) {
+        if (wait == 0) {
+            obj.removeAttribute("disabled");
+            obj.setAttribute("style", '');
+            obj.innerText = '';
+        } else {
+            obj.innerText = wait + "秒";
+            wait--;
+            setTimeout(function () {
+                timeDown(obj, wait)
+            }, 1000)
+        }
+    }
+});
 addTouchEvent(Dom.submit_btn, function () {
     // Dom.modal_loading.setAttribute('style', 'display: block');
     var tel = Dom.tel_input.value;
-    // if(!(/^1(3|4|5|7|8)\d{9}$/.test(tel))){
+    // if(!(Reg.tel.test(tel))){
     //     alert("手机号码有误，请重填");
     //     return false;
     // }
@@ -30,7 +58,7 @@ addTouchEvent(Dom.submit_btn, function () {
     //     }
     //     console.log(data);
     //     // Dom.modal_getted.setAttribute('style', 'display: block')
-        Dom.modal_suc.setAttribute('style', 'display: block');
+    //     Dom.modal_suc.setAttribute('style', 'display: block');
     //     // Dom.modal_over.setAttribute('style', 'display: block');
     // });
 });
