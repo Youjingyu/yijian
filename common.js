8619393af3eced5c2bfdadb5e138def990301e6f
youjingyu <735284268@qq.com>
2017-05-26 11:59:09 +0800

    添加优惠券显示逻辑，添加验证码不能为空的验证

--- common.js
+++ common.js
@@ -7,7 +7,8 @@ getEle('main').setAttribute('style', 'display: block');
 var scale = document.body.clientWidth / 1565;
 document.getElementsByTagName('body')[0].setAttribute('style', 'height:' + (scale * 3202)  + 'px');
 var Reg = {
-    tel: /^1(3|4|5|7|8)\d{9}$/
+    tel: /^1(3|4|5|7|8)\d{9}$/,
+    valid_num: /^\d{4}$/,
 }
 var Dom = {
     tel_input: getEle('tel_input'),
@@ -18,13 +19,14 @@ var Dom = {
     close_getted: getEle('close_getted'),
     modal_suc: getEle('modal_suc'),
     close_suc: getEle('close_suc'),
+    modal_suc_title: getEle('modal_suc_title'),
     modal_over: getEle('modal_over'),
     close_over: getEle('close_over'),
     modal_loading: getEle('modal_loading')
 }
 addTouchEvent(Dom.valid_num_btn, function () {
     if(!(Reg.tel.test(Dom.tel_input.value))){
-        alert("手机号码有误，请重填");
+        alert("手机号码有误，请重新填写");
         return false;
     }
     var btn = Dom.valid_num_btn;
@@ -46,21 +48,29 @@ addTouchEvent(Dom.valid_num_btn, function () {
     }
 });
 addTouchEvent(Dom.submit_btn, function () {
-    // Dom.modal_loading.setAttribute('style', 'display: block');
     var tel = Dom.tel_input.value;
-    // if(!(Reg.tel.test(tel))){
-    //     alert("手机号码有误，请重填");
-    //     return false;
-    // }
-    // http.post('http://192.168.0.230/eleme/users/add', tel, function (data) {
-    //     if(data.code == 0){
-    //
-    //     }
-    //     console.log(data);
-    //     // Dom.modal_getted.setAttribute('style', 'display: block')
-    //     Dom.modal_suc.setAttribute('style', 'display: block');
-    //     // Dom.modal_over.setAttribute('style', 'display: block');
-    // });
+    if(!(Reg.tel.test(tel))){
+        alert("手机号码有误，请重新填写");
+        return false;
+    }
+    if(!(Reg.valid_num.test(Dom.valid_num.value))){
+        alert("请输入正确的验证码");
+        return false;
+    }
+    Dom.modal_loading.setAttribute('style', 'display: block');
+    http.post('http://192.168.0.182/eleme/users/add', tel, function (data) {
+        Dom.modal_loading.setAttribute('style', '');
+        if(data.code == 40001){
+            Dom.modal_over.setAttribute('style', 'display: block');
+        } else if(data.code == 0){
+            Dom.modal_suc_title.setAttribute('class', 'modal-suc-title modal-suc-title-' + data.data);
+            Dom.modal_suc.setAttribute('style', 'display: block');
+        }
+        console.log(data);
+    //     Dom.modal_getted.setAttribute('style', 'display: block')
+    //     Dom.modal_over.setAttribute('style', 'display: block');
+    // Dom.modal_loading.setAttribute('style', 'display: block');
+    });
 });
 addTouchEvent(Dom.close_getted, function () {
     Dom.modal_getted.setAttribute('style', '')
@@ -101,7 +111,7 @@ function addTouchEvent(dom, callback) {
         xhr.onreadystatechange = function(){
             if (xhr.readyState == 4) {
                 if (xhr.status == 200) {
-                    success(xhr.responseText);
+                    success(JSON.parse(xhr.responseText));
                 }
             }
         }
